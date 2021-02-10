30bebb731c79e7665d833cfbb752955b550c3f2e
manacca <maximenaccache@gmail.com>
2021-02-10 10:17:20 +0100

    petite opti tow

--- philo_two/routine.c
+++ philo_two/routine.c
@@ -3,7 +3,7 @@
 void		*routine(void *p_data)
 {
 	t_philo *philo = p_data;
-	while (philo->no_limite == 0 && philo->nb_eat > 0)
+	while (philo->no_limite == 0 && philo->nb_eat > 0 && philo->perso->if_die == 0)
 	{
 		if (ft_check_die(philo) == 1)
 		{
@@ -21,38 +21,32 @@ void		*routine(void *p_data)
 		{
 			if (philo->perso->if_die == 1)
 				return (0);
-			if (philo->perso->fork / 2 >= 1)
+			philo->perso->fork -= 2;
+			sem_wait(philo->perso->s_fork);
+			display(philo->name_philo, " has taken a fork", philo);
+			display(philo->name_philo, " has taken a fork", philo);
+			if (philo->perso->if_die == 1)
 			{
-				philo->perso->fork -= 2;
-				sem_wait(philo->perso->s_fork);
-				display(philo->name_philo, " has taken a fork", philo);
-				display(philo->name_philo, " has taken a fork", philo);
-				if (philo->perso->if_die == 1)
-				{
-					sem_post(philo->perso->s_fork);
-					return (0);
-				}
-				philo_eat(philo);
-				philo->perso->fork += 2;
 				sem_post(philo->perso->s_fork);
-				if (philo->nb_eat > 0)
-				{
-					if (philo->perso->if_die == 1)
-						return (0);
-					display(philo->name_philo, " is sleeping", philo);
-					usleep(philo->time_sleep * 1000);
-					if (philo->perso->if_die == 1)
-						return (0);
-					display(philo->name_philo, " is thinking", philo);
-				}
+				return (0);
+			}
+			philo_eat(philo);
+			philo->perso->fork += 2;
+			sem_post(philo->perso->s_fork);
+			if (philo->nb_eat > 0)
+			{
+				if (philo->perso->if_die == 1)
+					return (0);
+				display(philo->name_philo, " is sleeping", philo);
+				usleep(philo->time_sleep * 1000);
+				if (philo->perso->if_die == 1)
+					return (0);
+				display(philo->name_philo, " is thinking", philo);
 			}
 		}
 	}
 	//
-	//
-	//
-	//
-	while (philo->no_limite == 1 && (philo->dead == 0 || philo->perso->if_die == 0))
+	while (philo->no_limite == 1 && philo->perso->if_die == 0)
 	{
 		if (ft_check_die(philo) == 1)
 		{
@@ -71,28 +65,25 @@ void		*routine(void *p_data)
 		{
 			if (philo->perso->if_die == 1)
 				return (0);
-			if (philo->perso->fork / 2 >= 1)
+			philo->perso->fork -= 2;
+			sem_wait(philo->perso->s_fork);
+			display(philo->name_philo, " has taken a fork", philo);
+			display(philo->name_philo, " has taken a fork", philo);
+			if (philo->perso->if_die == 1)
 			{
-				philo->perso->fork -= 2;
-				sem_wait(philo->perso->s_fork);
-				display(philo->name_philo, " has taken a fork", philo);
-				display(philo->name_philo, " has taken a fork", philo);
-				if (philo->perso->if_die == 1)
-				{
-					sem_post(philo->perso->s_fork);
-					return (0);
-				}
-				philo_eat(philo);
-				philo->perso->fork += 2;
 				sem_post(philo->perso->s_fork);
-				if (philo->perso->if_die == 1)
-					return (0);
-				display(philo->name_philo, " is sleeping", philo);
-				usleep(philo->time_sleep * 1000);
-				if (philo->perso->if_die == 1)
-					return (0);
-				display(philo->name_philo, " is thinking", philo);
+				return (0);
 			}
+			philo_eat(philo);
+			philo->perso->fork += 2;
+			sem_post(philo->perso->s_fork);
+			if (philo->perso->if_die == 1)
+				return (0);
+			display(philo->name_philo, " is sleeping", philo);
+			usleep(philo->time_sleep * 1000);
+			if (philo->perso->if_die == 1)
+				return (0);
+			display(philo->name_philo, " is thinking", philo);
 		}
 	}
 	return (0);
