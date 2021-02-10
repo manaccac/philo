9fb64df34b7e0333aac4c21942834e6610cfbab5
manacca <maximenaccache@gmail.com>
2021-02-10 09:39:56 +0100

    petite opti encore

--- philo_one/routine.c
+++ philo_one/routine.c
@@ -3,7 +3,7 @@
 void		*routine(void *p_data)
 {
 	t_philo *philo = p_data;
-	while (philo->no_limite == 0 && philo->nb_eat > 0)
+	while (philo->no_limite == 0 && philo->nb_eat > 0 && philo->perso->if_die == 0)
 	{
 		if (ft_check_die(philo) == 1)
 		{
@@ -23,12 +23,8 @@ void		*routine(void *p_data)
 			if ((philo->perso->fork_perso[philo->name_philo - 1] == 1 && philo->perso->fork_perso[philo->name_philo] == 1) ||
 				(philo->name_philo == 0 && (philo->perso->fork_perso[philo->nb_philo - 1] == 1 && philo->perso->fork_perso[philo->name_philo] == 1)))
 			{
-				if (philo->perso->if_die == 1)
-					return (0);
 				if (philo->perso->fork / 2 >= 1)
 				{
-					if (philo->perso->if_die == 1)
-						return (0);
 					philo->perso->fork_perso[philo->name_philo] = 0;
 					if (philo->name_philo != 0)
 						philo->perso->fork_perso[philo->name_philo - 1] = 0;
@@ -50,9 +46,8 @@ void		*routine(void *p_data)
 					}
 					philo_eat(philo);
 					pthread_mutex_unlock(&philo->perso->l_fork[philo->name_philo]);
-					philo->perso->fork += 1;
 					pthread_mutex_unlock(&philo->perso->r_fork[philo->name_philo]);
-					philo->perso->fork += 1;
+					philo->perso->fork += 2;
 					philo->perso->fork_perso[philo->name_philo] = 1;
 					if (philo->name_philo != 0)
 						philo->perso->fork_perso[philo->name_philo - 1] = 1;
@@ -70,15 +65,13 @@ void		*routine(void *p_data)
 					}
 				}
 			}
-			if (philo->perso->if_die == 1)
-				return (0);
 		}
 	}
 	//
 	//
 	//
 	//
-	while (philo->no_limite == 1 && (philo->dead == 0 || philo->perso->if_die == 0))
+	while (philo->no_limite == 1 && philo->perso->if_die == 0)
 	{
 		if (ft_check_die(philo) == 1)
 		{
@@ -97,12 +90,8 @@ void		*routine(void *p_data)
 			if ((philo->perso->fork_perso[philo->name_philo - 1] == 1 && philo->perso->fork_perso[philo->name_philo] == 1) ||
 				(philo->name_philo == 0 && (philo->perso->fork_perso[philo->nb_philo - 1] == 1 && philo->perso->fork_perso[philo->name_philo] == 1)))
 			{
-				if (philo->perso->if_die == 1)
-					return (0);
 				if (philo->perso->fork / 2 >= 1)
 				{
-					if (philo->perso->if_die == 1)
-						return (0);
 					philo->perso->fork_perso[philo->name_philo] = 0;
 					if (philo->name_philo != 0)
 						philo->perso->fork_perso[philo->name_philo - 1] = 0;
@@ -125,9 +114,8 @@ void		*routine(void *p_data)
 					philo_eat(philo);
 ////	///////////////////////////////
 					pthread_mutex_unlock(&philo->perso->l_fork[philo->name_philo]);
-					philo->perso->fork += 1;
 					pthread_mutex_unlock(&philo->perso->r_fork[philo->name_philo]);
-					philo->perso->fork += 1;
+					philo->perso->fork += 2;
 					philo->perso->fork_perso[philo->name_philo] = 1;
 					if (philo->name_philo != 0)
 						philo->perso->fork_perso[philo->name_philo - 1] = 1;
@@ -142,8 +130,6 @@ void		*routine(void *p_data)
 					display(philo->name_philo, " is thinking", philo);
 				}
 			}
-			if (philo->perso->if_die == 1)
-				return (0);
 		}
 	}
 	return (0);
