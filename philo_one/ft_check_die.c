3a1d8609967cfc89a5a3178e57e3ca5854d2813e
manacca <maximenaccache@gmail.com>
2021-02-18 09:21:01 +0100

    400 200 200 marche normal

--- philo_one/ft_check_die.c
+++ philo_one/ft_check_die.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_check_die.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdel-ros <jdel-ros@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: manaccac <manaccac@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 09:03:31 by jdel-ros          #+#    #+#             */
-/*   Updated: 2021/02/16 09:33:38 by jdel-ros         ###   ########lyon.fr   */
+/*   Updated: 2021/02/18 09:18:49 by manaccac         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_check_die(t_philo *philo)
 	struct timeval temp_dead;
 
 	gettimeofday(&temp_dead, NULL);
-	if (ft_conv_to_ms(temp_dead, philo->ms_died) > philo->time_die)
+	if (ft_conv_to_ms(temp_dead, philo->ms_died) >= philo->time_die)
 		return (1);
 	else
 		return (0);
