8dec93fd1789f27bf844cec8cc5412da0117a73a
manacca <maximenaccache@gmail.com>
2021-02-18 09:31:31 +0100

    je pense que ca marche

--- philo_three/proc.c
+++ philo_three/proc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   proc.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdel-ros <jdel-ros@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: manaccac <manaccac@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 13:59:47 by jdel-ros          #+#    #+#             */
-/*   Updated: 2021/02/17 14:44:23 by jdel-ros         ###   ########lyon.fr   */
+/*   Updated: 2021/02/18 09:30:39 by manaccac         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ int		proc(t_init *init, int nb_philo)
 	int i = 0;
 	int status = 0;
 
-	while (i < nb_philo)
+	while (i < nb_philo + 1)
 	{
-		init->philo[i].pid = fork();
-		if (init->philo[i].pid == -1)
+		init->philo[i + 1].pid = fork();
+		if (init->philo[i + 1].pid == -1)
 			exit(1);
-		if (init->philo[i].pid == 0)
+		if (init->philo[i + 1].pid == 0)
 		{
-			if (routine(&init->philo[i], init) == 0)
+			if (routine(&init->philo[i + 1], init) == 0)
 				exit(0);
 			else
 				exit(1);
@@ -46,11 +46,11 @@ int		proc(t_init *init, int nb_philo)
 			bl = 1;
 		//printf("%d : %d : %d \n", WEXITSTATUS(status), bl, y);
 		i = 0;
-		while (bl && i < nb_philo)
+		while (bl && i < nb_philo + 1)
 		{
 			// if (init->philo[i].pid == 0)
 			//dprintf(1, "pid = %d\n", init->philo[i].pid);
-			kill(init->philo[i].pid, 1);
+			kill(init->philo[i + 1].pid, 1);
 			i++;
 		}
 		u++;
